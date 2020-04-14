adfe0a9554341b85ca0d879dfdd3b80575b2456c
Caroline POTELLE <cpotelle@yahoo.fr>
2020-04-14 09:59:28 +0200

    E-mail unique

--- src/Entity/EterUser.php
+++ src/Entity/EterUser.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\EterUserRepository")
+ * @UniqueEntity("user_mail")
  */
 class EterUser implements UserInterface
 {

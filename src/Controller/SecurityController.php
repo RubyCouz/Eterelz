02ac9239ff8dc2c6f30178c1d7b8b0f15e655874
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-04-16 14:30:13 +0200

    Essai mail+Correction form inscr

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -39,6 +39,7 @@ class SecurityController extends AbstractController {
             //Récupération du champ User_Avatar de EterUser
             $file = $user->getUserAvatar();
 
+            if ($file != null){
             //Cryptage du nom de fichier téléchargé
             $fileName = md5(uniqid()).'.'.$file->getClientOriginalExtension();
 
@@ -84,7 +85,7 @@ class SecurityController extends AbstractController {
 
         $mailer->send($email);
     
-
+        }
             //return $this->redirectToRoute('/login');
 
         //Affichage
@@ -95,7 +96,7 @@ class SecurityController extends AbstractController {
     }
     }
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="login");
      * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */

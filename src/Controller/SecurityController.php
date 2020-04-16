c83a5c8e391734185a638fe0f6575bf97daa11a0
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-04-16 14:30:13 +0200

    Essai mail+Correction form inscr

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -39,6 +39,7 @@ class SecurityController extends AbstractController {
             //Récupération du champ User_Avatar de EterUser
             $file = $user->getUserAvatar();
 
+            if ($file != null){
             //Cryptage du nom de fichier téléchargé
             $fileName = md5(uniqid()).'.'.$file->getClientOriginalExtension();
 
@@ -84,9 +85,8 @@ class SecurityController extends AbstractController {
 
         $mailer->send($email);
     
-        
             //return $this->redirectToRoute('/login');
-
+        }
         //Affichage
         return $this->render('security/registration.html.twig', [
             'inProgress' => $inProgress,

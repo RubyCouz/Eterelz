eeceaf6b5e9803dfed4bf74f70cc72de1df0d54b
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-04-16 14:30:13 +0200

    Essai mail+Correction form inscr

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,6 +18,7 @@ class SecurityController extends AbstractController {
     /**
      * @Route("/inscription", name="security_registration")
      */
+    //Mailerinterface
     public function registration(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder) {
         
         //Définition de la variable en signalant que l'on veut créer un nouvel utilisateur
@@ -37,6 +38,7 @@ class SecurityController extends AbstractController {
             //Récupération du champ User_Avatar de EterUser
             $file = $user->getUserAvatar();
 
+            if ($file != null){
             //Cryptage du nom de fichier téléchargé
             $fileName = md5(uniqid()).'.'.$file->getClientOriginalExtension();
 
@@ -45,7 +47,7 @@ class SecurityController extends AbstractController {
 
             //Importation du fichier dans la BDD
             $user->setUserAvatar($fileName);
-
+            }
             //Encryptage du mot de passe selon la configuration dans security.yaml de config
             //Le premier paramètre détermine la façon de crypter, le second ce qu'il faut crypter
             $hash = $encoder->encodePassword($user, $user->getUserPassword());
@@ -58,6 +60,20 @@ class SecurityController extends AbstractController {
             
             //Envoi des données à la BDD
             $manager->flush();
+
+            $email = (new Email())
+                ->from('hello@example.com')
+                ->to('you@example.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+    
 
             //return $this->redirectToRoute('/login');
         }

22c6ea80c712377fe07660d4a775e62052cbdba5
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-04-17 10:19:49 +0200

    Début chantier barre mdp

--- public/js/script.js
+++ public/js/script.js
@@ -76,3 +76,24 @@ function socialOff() {
         facebook.style.display = 'block';
     };
 }
+    const socialIcon = document.getElementById('socialIcon');
+    console.log(socialIcon);
+    socialIcon.style.top = '200px';
+    socialIcon.style.right = '200px';
+    socialIcon.style.color = 'white';
+
+UIkit.util.ready(function () {
+
+    var bar = document.getElementById('mdp-progressbar');
+
+    var animate = setInterval(function () {
+
+        bar.value += 10;
+
+        if (bar.value >= bar.max) {
+            clearInterval(animate);
+        }
+
+    }, 1000);
+
+});

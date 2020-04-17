9e155e3cd14f015649e1528d8aadbf90115bdf83
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-04-17 17:07:40 +0200

    Cadre genre + début barre de mdp

--- public/js/script.js
+++ public/js/script.js
@@ -10,18 +10,46 @@ function changeCss() {
     socialIcon.style.right = '200px';
     socialIcon.style.color = 'white';
 }
-UIkit.util.ready(function () {
-
-    var bar = document.getElementById('mdp-progressbar');
-
-    var animate = setInterval(function () {
-
-        bar.value += 10;
-
-        if (bar.value >= bar.max) {
-            clearInterval(animate);
-        }
-
-    }, 1000);
-
-});
+var pass = document.getElementById("password")
+    pass.addEventListener('keyup', function() {
+        checkPassword(pass.value)
+    })
+    function checkPassword(password) {
+        var strengthBar = document.getElementById("strength")
+        var strength = 0;
+            if (password.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
+                strength += 1
+            }
+            if (password.match(/[~<>?]+/)) {
+                strength += 1
+            }
+            if (password.match(/[!@£$%^&*()]+/)) {
+                strength += 1
+            }
+            if (password.length >= 8) {
+                strength += 1
+            }
+    
+    switch (strength) {
+        case 1:
+            strengthBar.value = 20;
+            color = "red";
+            break
+        case 2:
+            strengthBar.value = 40;
+            color = "orange";
+            break
+        case 3:
+            strengthBar.value = 60;
+            color = "green";
+            break
+        case 4:
+            strengthBar.value = 80;
+            color = "green";
+            break
+        case 5:
+            strengthBar.value = 100;
+            color = "green";
+            break
+    }
+}

76fc109180ef3a5809b1a14150449a8003af17c1
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-04-17 12:36:16 +0200

    Insertion nouveautés page accueil

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -52,8 +52,7 @@ class RegistrationType extends AbstractType
             ->add('user_login', TextType::class, [
                 'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ']), new Regex(['message' => 'Caractère(s) non autorisé(s)', 'pattern' => '#[0-9a-zA-Zàâäéèêëïîôöùûüç!:.?,/]$#'])],
                 'attr' => ['placeholder' => 'Saisissez votre login', 'class' => 'uk-input'],
-                'label' => 'Login *',
-                //'label_attr' => ['class' => 'avatar']
+                'label' => 'Login *'
             ])
 
         //---------------------------------------------------EMAIL----------------------------------------------------------------//

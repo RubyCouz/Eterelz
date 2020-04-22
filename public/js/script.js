70494af39cd9f595e223f496212387f23efe2c5e
cpotelle <cpotelle@yahoo.fr>
2020-04-22 14:42:13 +0200

    Photo avatar

--- public/js/script.js
+++ public/js/script.js
@@ -184,3 +184,16 @@ var pass = document.getElementById("registration_user_password")
             break
     }
 }
+
+//Prévisualisation de la photo de profil
+
+function preview_image(event)
+{
+    var reader = new FileReader();
+    reader.onload = function()
+    {
+        var output = document.getElementById('output_image');
+        output.src = reader.result;
+    }
+reader.readAsDataURL(event.target.files[0]);
+}

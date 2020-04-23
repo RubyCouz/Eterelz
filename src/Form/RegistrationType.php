a074114c263c007aa6a9f7878e462637d2591b6b
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-04-23 17:38:22 +0200

    Désinstallation framework cody

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -21,17 +21,17 @@ class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        //---------------POUR POUVOIR SELECTIONNER LE SEXE---------------
+        //---------------CHOIX POUR GENRE---------------
         $choices=[
             'Masculin' => 'M',
             'Féminin' => 'F'
         ];
-
         $builder
-        //---------------AVATAR---------------//
-        //Demander si le nom d'avatar doit être unique
-        //Demander quels types de fichiers sont autorisés
-        //Demander la taille maximum de fichier autorisés
+        //---------------------------------------------------AVATAR----------------------------------------------------------------//
+
+        //Demander les types de fichier pour l'avatar qui seraient autorisés
+        //Demander la taille max de fichier autorisé
+
             ->add('user_avatar', FileType::class, [
                 'attr' => ['placeholder' => 'Choisissez un avatar', 'onchange' => 'preview_image(event)'],
                 'constraints' => [ new File([
@@ -43,58 +43,72 @@ class RegistrationType extends AbstractType
         //---------------------------------------------------LOGIN----------------------------------------------------------------//
 
             ->add('user_login', TextType::class, [
-                'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ']), new Regex(['message' => 'Caractère(s) non autorisé(s)', 'pattern' => '#[0-9a-zA-Zàâäéèêëïîôöùûüç!:_\-.?,/\#]$#'])],
+                'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ']), new Regex(['message' => 'Caractère(s) non autorisé(s)', 'pattern' => '#[0-9a-zA-Zàâäéèêëïîôöùûüç_\-!\#:.?,/]$#'])],
                 'attr' => ['placeholder' => 'Saisissez votre login']
             ])
-        //---------------MAIL---------------//
+
+        //---------------------------------------------------EMAIL----------------------------------------------------------------//
+
             ->add('user_mail', EmailType::class, [
                 'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ']), new Email(['message' => 'Adresse mail non valide !'])],
                 'attr' => ['placeholder' => 'Saisissez votre adresse mail']
             ])
-        //-------------PASSWORD-------------//
-        //Rechercher barre de force pour le mot de passe
-        //Exclure l'acceptation des caractères < et >
+
+        //---------------------------------------------------MOT DE PASSE---------------------------------------------------------//
             ->add('user_password', PasswordType::class, [
                 'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ']), new Regex(['message' => 'Mot de passe non valide !', 'pattern' => '#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$#'])],
                 'attr' => ['placeholder' => 'Saisissez un mot de passe', 'id' => 'password'],
                 'help' => 'Votre mot de passe doit contenir au moins une majuscule, une minuscule et des caractères spéciaux'
             ])
-        //---------CONFIRM PASSWORD---------//
+
+        //---------------------------------------------CONFIRMATION MOT DE PASSE--------------------------------------------------//
+
             ->add('confirm_user_password', PasswordType::class, [
                 'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ'])],
                 'attr' => ['placeholder' => 'Confirmez le mot de passe']
             ])
-        //-------------ADRESSE-------------//
+
+        //---------------------------------------------------ADRESSE--------------------------------------------------------------//
+
             ->add('user_address', TextType::class, [
                 'constraints' => [new Regex(['message' => 'Adresse non valide !', 'pattern' => '#[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)*#'])],
                 'attr' => ['placeholder' => 'Saisissez votre adresse']
             ])
-        //-----------CODE POSTAL-----------//
+
+        //---------------------------------------------------CODE POSTAL----------------------------------------------------------//
+
             ->add('user_zip', TextType::class, [
                 'constraints' => [new Regex(['message' => 'Code postal non valide !', 'pattern' => '#^[0-9]{5}$#'])],
                 'attr' => ['placeholder' => 'Saisissez votre code postal']
             ])
-        //--------------VILLE-------------//
+
+        //---------------------------------------------------VILLE----------------------------------------------------------------//
+
             ->add('user_city', TextType::class, [
                 'constraints' => [new Regex(['message' => 'Ville non valide !', 'pattern' => '#^[A-zA-ZéèîïÉÈÎÏ][A-zA-Zéèêàçîï]+([\'\s-][A-zA-ZéèîïÉÈÎÏ][A-zA-Zéèêàçîï])?#'])],
                 'attr' => ['placeholder' => 'Saisissez votre ville']
             ])
-        //------------DISCORD------------//
+
+        //----------------------------------------------DISCORD------------------------------------------------------------------//
+
             ->add('user_discord', TextType::class, [
                 'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ']), new Regex(['message' => 'ID Discord non valide !', 'pattern' => '#^\D+\#\d{4}$#'])],
                 'attr' => ['placeholder' => 'Saisissez votre ID Discord']
             ])
-        //-------------SEXE-------------//
+
+        //-------------------------------------------GENRE------------------------------------------------------------------//
+
             ->add('user_sex', ChoiceType::class, [
                 'choices' => $choices,
                 'expanded' => true,
                 'multiple' => false
             ])
-        //---------DESCRIPTION---------//
+
+        //-----------------------------------------DESCRIPTION---------------------------------------------------------------//
+
             ->add('user_description', TextareaType::class, [
                 'attr' => ['placeholder' => 'Saisissez une description']
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

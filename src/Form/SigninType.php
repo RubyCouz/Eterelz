5792fdb350dd2ef8c61570921c7bf983a3c09fcb
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2020-04-28 11:46:20 +0200

    commentaire ajax

--- src/Form/SigninType.php
+++ src/Form/SigninType.php
@@ -42,7 +42,9 @@ class SigninType extends AbstractType
             //---------------LOGIN---------------//
             ->add('user_login', TextType::class, [
                 'constraints' => [new NotBlank(['message' => 'Vous devez remplir ce champ']), new Regex(['message' => 'Caractère(s) non autorisé(s)', 'pattern' => '#[0-9a-zA-Zàâäéèêëïîôöùûüç!:_\-.?,/\#]$#'])],
-                'attr' => ['placeholder' => 'Saisissez votre login']
+                'attr' => ['placeholder' => 'Saisissez votre login',
+                    'class' => 'form-control form-control-sm validate'
+                ]
             ])
             //---------------MAIL---------------//
             ->add('user_mail', EmailType::class, [

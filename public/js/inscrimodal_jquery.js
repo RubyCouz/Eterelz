8381e1545df571c275de02c861f5f0ddb4c27d8e
Clément Dumas <dumasclementjulien@yahoo.fr>
2020-05-07 11:44:58 +0200

    Résolution bug modals inscr

--- public/js/inscrimodal_jquery.js
+++ public/js/inscrimodal_jquery.js
@@ -1,175 +1,191 @@
+// Fonction pour les messages d'erreurs du formulaire de connexion
+
 function verif() 
-{ 
-    var lemailconn = $("#emailconn").val();
-    var reg5 = /^[a-zA-Z0-9.-]+@[a-z0-9.-]{2,252}.[a-z]{2,4}$/;
-    
-        if (lemailconn === "") 
-            {            
-                var html ="<div class='alert alert-danger'role='alert'>"+"Veuillez saisir votre login !"+"</div>";
-                $("#alertemailconn").append(html);
-            }
+{     
+// Récupère la valeur saisie dans le champ input      
+     
+     var mail = $("#mail").val();
+     var mail_v = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,252}\.[a-z]{2,4}$/;
+     var mdp = $("#mdp").val();
+     var mdp_v = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$/;
+     
+// On teste si la valeur est bonne
 
-        else if (reg5.test(lemailconn) == false) 
-            {
-                var html ="<div class='alert alert-warning'role='alert'>"+"Caractère(s) non autorisé(s) !"+"</div>";
-                $("#alertemailconn").append(html);
-            }
-            
-        else
-            {
-                var html ="<div class='alert alert-success'role='alert'>"+"Ok !"+"</div>";
-                $("#alertemailconn").append(html);
-            }
+     // EMAIL
 
-    var lemdpconn = $("#mdpconn").val();
-    var reg6 = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$/;
-    
-        if (lemdpconn === "") 
-            {            
-                var html ="<div class='alert alert-danger'role='alert'>"+"Veuillez saisir votre mot de passe !"+"</div>";
-                $("#alertmdpconn").append(html);
-                return false;
-            }
-        else if (reg6.test(lemdpconn) == false) 
-            {
-                var html ="<div class='alert alert-warning'role='alert'>"+"Format incorrect !"+"</div>";
-                $("#alertmdpconn").append(html);
-                return false;
-            }
-        else
-            {
-                var html ="<div class='alert alert-success'role='alert'>"+"Ok !"+"</div>";
-                $("#alertmdpconn").append(html);
-            }
+     if (mail === "") 
+     {            
+        var html = '<div class="alert alert-danger" role="alert">Votre email doit être renseigné !</div>';
+        $("#alert6").append(html); 
+     }
+     else if (mail_v.test(mail) == false)
+     {
+        var html = '<div class="alert alert-warning" role="alert">Format non valide !</div>';
+        $("#alert6").append(html);
+     }
+     else
+     {
+         var html = '<div class="alert alert-success" role="alert">Ok</div>';
+         $("#alert6").append(html);
+     }
 
+      // PASSWORD
+
+     if (mdp === "") 
+     {            
+        var html = '<div class="alert alert-danger" role="alert">Veuillez saisir votre mot de passe !</div>';
+        $("#alert7").append(html); 
+        return false;
+     }
+     else if (mdp_v.test(mdp) == false)
+     {
+        var html = '<div class="alert alert-warning" role="alert">Format non valide !</div>';
+        $("#alert7").append(html);
+        return false;
+     }
+     else
+     {
+         var html = '<div class="alert alert-success" role="alert">Ok</div>';
+         $("#alert7").append(html);
+     }
+
+    // Si aucun test n'a renvoyé faux, c'est qu'il n'y a pas d'erreur, le script arrive ici, le formulaire est envoyé via submit()
     document.forms[0].submit();
-
 }
 
-$("#bouton_envoi_modalconn").click(function(event) 
+     $("#bouton_envoi1").click(function(event) 
 {
-    /* On doit bloquer l'èvènement par défaut - ici l'envoi du formulaire - avec l'instruction preventDefault() 
-    * le paramètre 'event' est un objet (nommé librement) représentant l'évènement
-    */ 
-
+    /* 
+    On doit bloquer l'èvènement par défaut - ici l'envoi du formulaire avec l'instruction preventDefault(). Le paramètre 'event' est un objet (nommé librement) représentant l'évènement
+    */         
     event.preventDefault();
-
+ 
     // Appel de la fonction verif()
     verif();             
-}); 
+});
+
+// Fonction pour les messages d'erreurs du formulaire d'inscription
 
 function verif2() 
-{ 
-    var lesurnom = $("#surnom").val();
-    var reg1 = /^[0-9a-zA-Zàâäéèêëïîôöùûüç!:_\-.?,\/#]+$/;
-    
-        if (lesurnom === "") 
-            {            
-                var html ="<div class='alert alert-danger'role='alert'>"+"Veuillez créer un login !"+"</div>";
-                $("#alertsurnom").append(html);
-            }
+{     
+// Récupère la valeur saisie dans le champ input      
+     
+     var identifiant = $("#identifiant").val();
+     var identifiant_v = /^[0-9a-zA-Zàâäéèêëïîôöùûüç!:_\-.?,\/#]+$/;
+     var email = $("#email").val();
+     var email_v = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,252}\.[a-z]{2,4}$/;
+     var discord = $("#discord").val();
+     var discord_v = /^\D+\#\d{4}$/
+     var password = $("#password").val();
+     var password_v = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$/;
+     var conf_password = $("#conf_password").val();
+     
+// On teste si la valeur est bonne
 
-        else if (reg1.test(lesurnom) == false) 
-            {
-                var html ="<div class='alert alert-warning'role='alert'>"+"Format incorrect !"+"</div>";
-                $("#alertsurnom").append(html);
-            }
-            
-        else
-            {
-                var html ="<div class='alert alert-success'role='alert'>"+"Ok !"+"</div>";
-                $("#alertsurnom").append(html);
-            }
+    // LOGIN
 
-    var lemail = $("#email").val();
-    var reg2 = /^[a-zA-Z0-9.-]+@[a-z0-9.-]{2,252}.[a-z]{2,4}$/;
-    
-        if (lemail === "") 
-            {            
-                var html ="<div class='alert alert-danger'role='alert'>"+"Vous n'avez pas renseigné votre email !"+"</div>";
-                $("#alertemail").append(html);
-            }
-        else if (reg2.test(lemail) == false) 
-            {
-                var html ="<div class='alert alert-warning'role='alert'>"+"Format incorrect !"+"</div>";
-                $("#alertemail").append(html);
-            }
-        else
-            {
-                var html ="<div class='alert alert-success'role='alert'>"+"Ok !"+"</div>";
-                $("#alertemail").append(html);
-            }
+    if (identifiant === "") 
+    {            
+        var html = '<div class="alert alert-danger" role="alert">Veuillez créer un login !</div>';
+        $("#alert1").append(html); 
+    }
+    else if (identifiant_v.test(identifiant) == false)
+    {
+        var html = '<div class="alert alert-warning" role="alert">Caractère(s) non autorisé(s) !</div>';
+        $("#alert1").append(html);
+    }
+    else
+    {
+        var html = '<div class="alert alert-success" role="alert">Ok</div>';
+        $("#alert1").append(html);
+    }
 
-    var lediscord = $("#discord").val();
-    var reg3 = /^\D+#\d{4}$/;
-    
-        if (lediscord === "") 
-            {            
-                var html ="<div class='alert alert-danger'role='alert'>"+"Vous n'avez pas renseigné votre ID Discord !"+"</div>";
-                $("#alertedisc").append(html);
-            }
-        else if (reg3.test(lediscord) == false) 
-            {
-                var html ="<div class='alert alert-warning'role='alert'>"+"Format incorrect !"+"</div>";
-                $("#alertedisc").append(html);
-            }
-        else
-            {
-                var html ="<div class='alert alert-success'role='alert'>"+"Ok !"+"</div>";
-                $("#alertedisc").append(html);
-            }
+     // EMAIL
 
-    var lemdp = $("#mdp").val();
-    var reg4 = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$/;
-    
-        if (lemdp === "") 
-            {            
-                var html ="<div class='alert alert-danger'role='alert'>"+"Vous n'avez pas renseigné votre mot de passe !"+"</div>";
-                $("#alertemdp").append(html);
-            }
-        else if (reg4.test(lemdp) == false) 
-            {
-                var html ="<div class='alert alert-warning'role='alert'>"+"Format incorrect !"+"</div>";
-                $("#alertemdp").append(html);
-            }
-        else
-            {
-                var html ="<div class='alert alert-success'role='alert'>"+"Ok !"+"</div>";
-                $("#alertemdp").append(html);
-            }
+     if (email === "") 
+     {            
+        var html = '<div class="alert alert-danger" role="alert">Votre email doit être renseigné !</div>';
+        $("#alert2").append(html); 
+     }
+     else if (email_v.test(email) == false)
+     {
+        var html = '<div class="alert alert-warning" role="alert">Format non valide !</div>';
+        $("#alert2").append(html);
+     }
+     else
+     {
+         var html = '<div class="alert alert-success" role="alert">Ok</div>';
+         $("#alert2").append(html);
+     }
 
-    var leconfmdp = $("#confmdp").val();
-    
-        if (leconfmdp === "") 
-            {            
-                var html ="<div class='alert alert-danger'role='alert'>"+"Vous n'avez pas confirmé votre mot de passe !"+"</div>";
-                $("#alerteconfmdp").append(html);
-                return false;
-            }
-        else if (leconfmdp !== lemdp) 
-            {
-                var html ="<div class='alert alert-warning'role='alert'>"+"Vos mots de passe sont différents !"+"</div>";
-                $("#alerteconfmdp").append(html);
-                return false;
-            }
-        else
-            {
-                var html ="<div class='alert alert-success'role='alert'>"+"Ok !"+"</div>";
-                $("#alerteconfmdp").append(html);
-            }
+      // ID DISCORD
 
+      if (discord === "") 
+      {            
+         var html = '<div class="alert alert-danger" role="alert">Veuillez créer un ID Discord !</div>';
+         $("#alert3").append(html); 
+      }
+      else if (discord_v.test(discord) == false)
+      {
+         var html = '<div class="alert alert-warning" role="alert">Format non valide !</div>';
+         $("#alert3").append(html);
+      }
+      else
+      {
+          var html = '<div class="alert alert-success" role="alert">Ok</div>';
+          $("#alert3").append(html);
+      }
+
+      // PASSWORD
+
+     if (password === "") 
+     {            
+        var html = '<div class="alert alert-danger" role="alert">Veuillez créer un mot de passe !</div>';
+        $("#alert4").append(html); 
+     }
+     else if (password_v.test(password) == false)
+     {
+        var html = '<div class="alert alert-warning" role="alert">Format non valide !</div>';
+        $("#alert4").append(html);
+     }
+     else
+     {
+         var html = '<div class="alert alert-success" role="alert">Ok</div>';
+         $("#alert4").append(html);
+     }
+
+     // CONFIRMATION PASSWORD
+
+     if (conf_password === "") 
+     {            
+        var html = '<div class="alert alert-danger" role="alert">Veuillez confirmer votre mot de passe !</div>';
+        $("#alert5").append(html);
+        return false;
+     }
+     else if (conf_password !== password)
+     {
+        var html = '<div class="alert alert-warning" role="alert">Les deux mots de passe sont différents !</div>';
+        $("#alert5").append(html);
+        return false;
+     }
+     else
+     {
+         var html = '<div class="alert alert-success" role="alert">Ok</div>';
+         $("#alert5").append(html);
+     }
+
+
+    // Si aucun test n'a renvoyé faux, c'est qu'il n'y a pas d'erreur, le script arrive ici, le formulaire est envoyé via submit()
     document.forms[1].submit();
-
 }
 
-$("#bouton_envoi_modal").click(function(event) 
+     $("#bouton_envoi2").click(function(event) 
 {
-    /* On doit bloquer l'èvènement par défaut - ici l'envoi du formulaire - avec l'instruction preventDefault() 
-    * le paramètre 'event' est un objet (nommé librement) représentant l'évènement
-    */ 
-
+    /* 
+    On doit bloquer l'èvènement par défaut - ici l'envoi du formulaire avec l'instruction preventDefault(). Le paramètre 'event' est un objet (nommé librement) représentant l'évènement
+    */         
     event.preventDefault();
-
+ 
     // Appel de la fonction verif()
     verif2();             
-}); 
+});

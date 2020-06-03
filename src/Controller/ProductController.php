f628ae0794d13d4a0c06d263375992ada4611015
cpotelle <cpotelle@yahoo.fr>
2020-06-03 16:50:23 +0200

    E-commerce en cours

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EterProductRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product_index")
+     */
+    public function index(EterProductRepository $eterProductRepository)
+    {
+        $inProgress = false;
+        return $this->render('product/list.html.twig', [
+            'products' => $eterProductRepository->findAll(),
+            'inProgress' => $inProgress
+        ]);
+    }
+}
---
+++ src/Entity/EterProduct.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EterProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EterProductRepository::class)
+ */
+class EterProduct
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $product_title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $product_image;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $product_price;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $product_quantity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductTitle(): ?string
+    {
+        return $this->product_title;
+    }
+
+    public function setProductTitle(string $product_title): self
+    {
+        $this->product_title = $product_title;
+
+        return $this;
+    }
+
+    public function getProductImage(): ?string
+    {
+        return $this->product_image;
+    }
+
+    public function setProductImage(?string $product_image): self
+    {
+        $this->product_image = $product_image;
+
+        return $this;
+    }
+
+    public function getProductPrice(): ?int
+    {
+        return $this->product_price;
+    }
+
+    public function setProductPrice(int $product_price): self
+    {
+        $this->product_price = $product_price;
+
+        return $this;
+    }
+
+    public function getProductQuantity(): ?int
+    {
+        return $this->product_quantity;
+    }
+
+    public function setProductQuantity(?int $product_quantity): self
+    {
+        $this->product_quantity = $product_quantity;
+
+        return $this;
+    }
+}

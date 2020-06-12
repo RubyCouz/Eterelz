de7a23b7404e7c569c8fdd887cf6583118c289fe
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2020-06-12 13:13:03 +0200

    fix cart js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,14 +1,17 @@
 // récupération des éléments
-let plus = document.getElementsByClassName('plus');
-let minus = document.getElementsByClassName('minus');
-let qty = document.getElementsByClassName('qty');
+ let plus = document.querySelector('.plus');
+console.log(document.querySelector('.plus'));
+
+ let minus = document.querySelector('.minus');
+ let qty = document.querySelector('.qty');
 
 // récupération de la valeur dans l'input
-let inputQty = document.getElementsByClassName('inputQty').value;
+ let inputQty = document.querySelector('.inputQty').value;
 
 // évènement
-plus.onclick = cartupdateplus;
-minus.onclick = cartupdatemoins;
+
+// plus.click = cartupdateplus;
+// minus.onclick = cartupdatemoins;
 
 /*
  action déclencher lors des events
@@ -17,16 +20,19 @@ minus.onclick = cartupdatemoins;
 function cartupdateplus(){
     let qtyValor = parseInt(inputQty);
     qtyValor += 1; // qtyValor ++
-
+    inputQty = qtyValor;
     qty.innerHTML = qtyValor;
-
+console.log(qtyValor);
 }
 
 // décrémentation article
 function cartupdatemoins(){
     let qtyValor = parseInt(inputQty);
     qtyValor -= 1; // qtyValor--
-
+    inputQty = qtyValor;
     qty.innerHTML = qtyValor;
 
 }
+
+plus.addEventListener('click', cartupdateplus);
+minus.addEventListener('click', cartupdatemoins);

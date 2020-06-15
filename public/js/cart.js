cc1049e656fac8262ab9d4604997ea5405e9a189
BaltoClem <dumasclementjulien@yahoo.fr>
2020-06-15 12:49:39 +0200

    Navbar fix + Quantité panier js en cours

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,12 +1,14 @@
 // récupération des éléments
 let plus = document.querySelector('.plus');
-console.log(document.querySelector('.plus'));
-
 let minus = document.querySelector('.minus');
 let qty = document.querySelector('.qty');
+let subtotal = document.querySelector('.subtotal');
+//let priceunit = document.querySelector('.priceUnit')
 
 // récupération de la valeur dans l'input
 let inputQty = document.querySelector('.inputQty').value;
+let inputSubtotal =  document.querySelector('.inputSubtotal').value;
+let inputPrice = document.querySelector('.inputPrice').value;
 
 // évènement
 
@@ -22,7 +24,12 @@ function cartupdateplus(){
     qtyValor += 1; // qtyValor ++
     inputQty = qtyValor;
     qty.innerHTML = qtyValor;
-    console.log(qtyValor);
+    if(qtyValor > 0){
+        let priceValor = parseFloat(inputPrice);
+        inputSubtotal = parseFloat(priceValor) * parseFloat(qtyValor);
+        //console.log(inputSubtotal.toFixed(2));
+        subtotal.innerHTML = inputSubtotal+" &euro;";
+    }
 }
 
 // décrémentation article
@@ -32,6 +39,18 @@ function cartupdatemoins(){
     inputQty = qtyValor;
     qty.innerHTML = qtyValor;
 
+        if(qtyValor < 0){
+            inputQty = 0;
+            qty.innerHTML = 0;
+        }
+        else if(qtyValor === 0){
+           confirm('Voulez-vous supprimer le produit du panier');
+        }
+        else if(qtyValor > 0){
+            let priceValor = parseFloat(inputPrice);
+            inputSubtotal = parseFloat(priceValor) * parseFloat(qtyValor);
+            subtotal.innerHTML = inputSubtotal+" &euro;";
+        }
 }
 
 plus.addEventListener('click', cartupdateplus);
